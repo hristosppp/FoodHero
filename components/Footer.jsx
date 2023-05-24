c4730459e3971a8ae2d851f8cdeaf703944ba29d
Hristos Pappasgit config --global user.name Hristos <hristos.pappas@icloud.com>
2023-05-24 20:06:11 +0800

    implemented the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,15 @@
+import { SocialIcon } from "react-social-icons";
 
 export default function Footer() {
     return (
-      <div className="d-flex justify-content-center fixed-bottom text-secondary bg-danger mt-50">
-        <h1>Footer</h1>
+      <div className="d-flex justify-content-center align-items-center flex-column fixed-bottom text-secondary bg-danger mt-50">
+        <div className="text-primary">
+          created by <strong>Hristos Pappas</strong>
+        </div>
+        <div className="m-2">
+        <SocialIcon style={{height: "30px", width: "30px", marginRight: "7px"}} url="https://www.linkedin.com/in/hristos-pappas-447a92274/" />
+        <SocialIcon style={{height: "30px", width: "30px"}} url="https://github.com/hristosppp" />
+        </div>
       </div>
     )
   }
